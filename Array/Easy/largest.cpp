2fa7f2494b61a7e2569af6ad80458d0d71b97a54
Amisha <amishashrivastava998@gmail.com>
2022-11-17 20:56:41 +0530

    second large element program added

--- Array/Easy/largest.cpp
+++ Array/Easy/largest.cpp
@@ -1,25 +1,38 @@
 #include <bits/stdc++.h>
  
 using namespace std;
-int findLargestElement(int arr[], int n) {
- 
-  int max = arr[0];
-  for (int i = 0; i < n; i++) {
-    if (max < arr[i]) {
-      max = arr[i];
+int findSecondLargestElement(int arr[], int n) {
+  
+  int large = arr[0];
+  int secondLarge = arr[1];
+  if(large < secondLarge){
+    swap(large,secondLarge);
+  }
+
+  for (int i = 2; i < n; i++) {
+    if (arr[i]<large && arr[i]>secondLarge) {
+      secondLarge = arr[i];
+    }
+    else if (arr[i]>large){
+        secondLarge = large;
+        large = arr[i];
     }
   }
-  return max;
+  if(large == secondLarge){
+    return -1;
+  }
+  else
+  return secondLarge;
 }
 int main() {
-  int arr1[] = {2,5,1,3,0};
+  int arr1[] = {5, 5, 5, 5, 5};
   int n = 5;
-  int max = findLargestElement(arr1, n);
-  cout << "The largest element in the array is: " << max << endl;
+  int max = findSecondLargestElement(arr1, n);
+  cout << "The second largest element in the array is: " << max << endl;
  
   int arr2[] =  {8,10,5,7,9};
   n = 5;
-  max = findLargestElement(arr2, n);
-  cout << "The largest element in the array is: " << max<<endl;
+  max = findSecondLargestElement(arr2, n);
+  cout << "The largest second element in the array is: " << max<<endl;
   return 0;
 }
